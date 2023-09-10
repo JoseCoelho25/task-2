feaa7b4c3294eee39c0682ba3059e4502a17aa3d
Jose Coelho <zenani87@hotmail.com>
2023-09-10 17:54:19 +0100

    adjusted display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import * as jsonData from '../app/data/sidebar_default (1).json'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'task-2';
+  //Store data from json file
+  data: any = jsonData;
+
+// Create an empty Record to store subcategories.
+  subcategoriesMap: Record<number, any> = {};
+
+  ngOnInit() {
+    console.log('Data', this.data);
+
+    // Iterate through each category and its subcategories.
+    this.data.categories.forEach((category: any) => {
+      category.subcategories.forEach((subcategory: any) => {
+        // Use the subcategory ID as the key and store the subcategory object in the Record.
+        this.subcategoriesMap[subcategory.id] = subcategory;
+
+      });
+    });
+
+    console.log('Subcategories Map', this.subcategoriesMap);
+
+  }
 }
